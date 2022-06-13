c13ce1ecc01a1e2bd7eb6b41c4ebf88c97a5cc69
abhinav3256 <abhinavkumar3256@gmail.com>
2022-06-13 13:52:28 +0530

    adding prime number file

---
+++ prime_number.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("test")
+
+	for a := 3; a <= 100; a++ {
+		flag := 0
+		for i := 2; i < 100; i++ {
+			if a != i {
+				if a%i == 0 {
+					flag = 1
+
+				}
+			}
+		}
+		if flag == 1 {
+			//fmt.Println("Not a prime number")
+		} else {
+			fmt.Println(a)
+		}
+	}
+}
